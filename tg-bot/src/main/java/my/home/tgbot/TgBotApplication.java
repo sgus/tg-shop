eee306c51768943e7e067b399cc993194feb3aae
sgus <g.usatov@gmail.com>
2022-10-19 20:51:16 +0300

    Инициализировал проект, добавил проперти

---
+++ tg-bot/src/main/java/my/home/tgbot/TgBotApplication.java
@@ -0,0 +1,15 @@
+package my.home.tgbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableEurekaClient
+public class TgBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TgBotApplication.class, args);
+	}
+
+}
